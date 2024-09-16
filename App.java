a6847ab0ce6decea727f485c67d88f484a9019bd
Rafael Bohrer Ávila <rafaelavila@ifsul.edu.br>
2024-09-16 19:47:28 +0000

    Agora usando ArrayList em vez de array normal

--- App.java
+++ App.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // criar o teclado
         Scanner teclado = new Scanner(System.in);
 
-        Pet[] pets = new Pet[100]; // estimativa de tamanho suficiente...
-        int contPets = 0;
+        ArrayList<Pet> pets = new ArrayList<>();
         while (true) {
             // mostrar o menu
             System.out.println("\nEscolha uma opção:");
@@ -38,12 +38,12 @@ public class App {
                 Pet p = new Pet(nome, tipo, idade, dono, telefone);
     
                 // adicionar o novo Pet à lista
-                pets[contPets++] = p;
+                pets.add(p);
             } else if (escolha == 2) {
                 // se opção = 2
                 System.out.println("\nPets cadastrados:"); 
-                for (int i = 0; i < contPets; i++) {
-                    System.out.println(pets[i]);
+                for (Pet p: pets) {
+                    System.out.println(p);
                 }
             } else {
                 // se opção = 3
